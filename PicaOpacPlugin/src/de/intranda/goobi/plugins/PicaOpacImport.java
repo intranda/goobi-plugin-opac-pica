2ed8fd4cdb58bd60200edfb7d7872aef0737779d
Robert Sehr <robert.sehr@intranda.com>
2014-07-15 09:16:06 +0200

    removed debug output

--- PicaOpacPlugin/src/de/intranda/goobi/plugins/PicaOpacImport.java
+++ PicaOpacPlugin/src/de/intranda/goobi/plugins/PicaOpacImport.java
@@ -197,10 +197,10 @@ public class PicaOpacImport implements IOpacPlugin {
          * -------------------------------- aus Opac-Ergebnis RDF-Datei erzeugen --------------------------------
          */
         /* XML in Datei schreiben */
-        		 XMLOutputter outputter = new XMLOutputter();
-        		 FileOutputStream output = new
-        		 FileOutputStream("/home/robert/temp_opac.xml");
-        		 outputter.output(myJdomDoc.getRootElement(), output);
+//        		 XMLOutputter outputter = new XMLOutputter();
+//        		 FileOutputStream output = new
+//        		 FileOutputStream("/home/robert/temp_opac.xml");
+//        		 outputter.output(myJdomDoc.getRootElement(), output);
 
         /* myRdf temporär in Datei schreiben */
         // myRdf.write("D:/temp.rdf.xml");
